35725015f567226dad7191459e4e8dacd6874c0b
Akash8377 <akashchoudhary8377@gmail.com>
2024-08-28 16:08:46 +0530

    grid body

--- controllers/grid.js
+++ controllers/grid.js
@@ -1,31 +1,26 @@
 const { validationResult } = require("express-validator");
+require("dotenv").config();
 const conn = require("../services/db");
 
 exports.get = (req, res) => {
   let query = `
     SELECT
-      g.id,
-      g.expected_value,
-      g.win_percentage,
-      g.major_percentage,
-      g.team,
-      g.future,
-      g.status,
-      w.id AS week_id,
-      w.week_name,
-      w.spread,
-      o.id AS opponent_id,
-      o.name AS opponent_name
+      h.id,
+      h.expected_value,
+      h.win_percentage,
+      h.major_percentage,
+      h.team,
+      h.future,
+      h.week_id as weekId,
+      h.status,
+      GROUP_CONCAT(l.week_name) AS week,
+      GROUP_CONCAT(l.id) AS weekIds
     FROM
-      grid g
+      grid h
     LEFT JOIN
-      week w ON FIND_IN_SET(w.id, g.week_id)
-    LEFT JOIN
-      week_opponents wo ON w.id = wo.week_id
-    LEFT JOIN
-      opponents o ON wo.opponent_id = o.id
+      week l ON FIND_IN_SET(l.id, h.week_id)
     GROUP BY
-      g.id, w.id, o.id;
+      h.id, h.expected_value, h.win_percentage, h.major_percentage, h.team, h.future;
   `;
   
   conn.query(query, (error, results) => {
@@ -35,52 +30,27 @@ exports.get = (req, res) => {
         msg: "Internal Server Error",
       });
     } else {
-      const grid = results.reduce((acc, row) => {
-        const existingGridItem = acc.find(item => item.id === row.id);
-        const weekInfo = {
-          id: row.week_id,
-          name: row.week_name,
-          spread: row.spread,
-        };
-        const opponentInfo = {
-          id: row.opponent_id,
-          name: row.opponent_name,
-        };
-
-        if (existingGridItem) {
-          const existingWeek = existingGridItem.weeks.find(w => w.id === row.week_id);
-          if (existingWeek) {
-            if (opponentInfo.id) {
-              existingWeek.opponents.push(opponentInfo);
-            }
-          } else {
-            existingGridItem.weeks.push({
-              ...weekInfo,
-              opponents: opponentInfo.id ? [opponentInfo] : [],
-            });
-          }
-        } else {
-          acc.push({
-            id: row.id,
-            expected_value: row.expected_value,
-            win_percentage: row.win_percentage,
-            major_percentage: row.major_percentage,
-            team: row.team,
-            future: row.future,
-            status: row.status,
-            weeks: [{
-              ...weekInfo,
-              opponents: opponentInfo.id ? [opponentInfo] : [],
-            }],
-          });
-        }
-
-        return acc;
-      }, []);
+      const grid = results.map((row) => ({
+        id: row.id,
+        expected_value: row.expected_value,
+        win_percentage: row.win_percentage,
+        major_percentage: row.major_percentage,
+        team: row.team,
+        future: row.future,
+        status: row.status,
+        week_id: row.weekId,
+        week: row.week,
+        week_info: row.weekIds
+          ? row.weekIds.split(",").map((weekId, index) => ({
+              id: weekId,
+              name: row.week.split(",")[index],
+            }))
+          : [],
+      }));
 
       res.status(200).send({
         status: "success",
-        length: grid.length,
+        length: results.length,
         data: grid,
       });
     }
@@ -93,67 +63,68 @@ exports.register = (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  let inputArray = req.body.week_id;
-
-  if (!Array.isArray(inputArray)) {
-    inputArray = [inputArray];
-  }
-
+  const inputArray = req.body.week_id; // Assuming week_id is an array
   const commaSeparatedString = inputArray.join(",");
   const date_time = new Date();
 
-  conn.query(
-    `SELECT * FROM grid WHERE LOWER(team) = LOWER(${conn.escape(req.body.team)});`,
-    (err, result) => {
-      if (result && result.length) {
-        return res.status(409).send({
-          msg: "This grid item already exists",
-        });
-      } else {
-        const sqlQuery = `
-          INSERT INTO grid (expected_value, win_percentage, major_percentage, team, future, week_id, status, created_at, updated_at)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        `;
-        const values = [
-          req.body.expected_value,
-          req.body.win_percentage,
-          req.body.major_percentage,
-          req.body.team,
-          req.body.future,
-          commaSeparatedString,
-          req.body.status,
-          date_time,
-          date_time,
-        ];
-        
-        conn.query(sqlQuery, values, (err, result) => {
-          if (err) {
-            return res.status(500).send({
-              msg: err,
-            });
-          } else {
-            res.status(200).send({
-              status: "success",
-              msg: "Grid item registration successful",
-            });
-          }
-        });
-      }
+  const sqlCheckQuery = `SELECT * FROM grid WHERE LOWER(team) = LOWER(${conn.escape(req.body.team)});`;
+
+  conn.query(sqlCheckQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send({
+        msg: "Internal Server Error",
+      });
     }
-  );
+
+    if (result && result.length) {
+      return res.status(409).send({
+        msg: "This grid item already exists",
+      });
+    } else {
+      const sqlQuery = `
+        INSERT INTO grid (expected_value, win_percentage, major_percentage, team, future, week_id, status, created_at, updated_at)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+      const values = [
+        req.body.expected_value,
+        req.body.win_percentage,
+        req.body.major_percentage,
+        req.body.team,
+        req.body.future,
+        commaSeparatedString,
+        req.body.status,
+        date_time,
+        date_time,
+      ];
+      
+      conn.query(sqlQuery, values, (err, result) => {
+        if (err) {
+          return res.status(500).send({
+            msg: err.message,
+          });
+        } else {
+          res.status(200).send({
+            status: "success",
+            msg: "Grid item registration successful",
+          });
+        }
+      });
+    }
+  });
 };
 
 exports.edit = (req, res) => {
-  let sqlQuery = "SELECT * FROM grid WHERE id=" + req.params.id;
-  conn.query(sqlQuery, (err, result) => {
+  const sqlQuery = `SELECT * FROM grid WHERE id = ?`;
+
+  conn.query(sqlQuery, [req.params.id], (err, result) => {
     if (err) {
       return res.status(500).send({
-        msg: err,
+        msg: err.message,
       });
     } else {
       res.status(200).send({
         status: "success",
-        length: result?.length,
+        length: result.length,
         data: result,
       });
     }
@@ -208,7 +179,7 @@ exports.delete = (req, res) => {
   conn.query(sqlQuery, [req.params.id], (err, result) => {
     if (err) {
       return res.status(500).send({
-        msg: err,
+        msg: err.message,
       });
     } else {
       res.status(200).send({
@@ -222,7 +193,7 @@ exports.delete = (req, res) => {
 exports.status = (req, res) => {
   const status = req.body.status; // Should be "active" or "inactive"
   const id = req.params.id;
-  const sqlQuery = `UPDATE grid SET status = ? WHERE id = ?;`;
+  const sqlQuery = `UPDATE grid SET status = ? WHERE id = ?`;
 
   conn.query(sqlQuery, [status, id], (err, result) => {
     if (err) {
