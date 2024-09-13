c599fcfd8a371e010b9df2289d2ad516d5172845
Akash8377 <akashchoudhary8377@gmail.com>
2024-09-13 10:26:08 +0530

    port change

--- services/db.js
+++ services/db.js
@@ -1,22 +1,11 @@
 const mysql = require("mysql");
 const conn = mysql.createConnection({
   host: "database-1.c54c86qsuhll.us-east-1.rds.amazonaws.com",
-  port: 3306, 
+  port: 3306,
   user: "admin",
   password: "spreadsandlocks",
   database: "bagels",
 });
-
-conn.connect((err) => {
-  if (err) {
-    console.error("Database connection error:", err.stack);
-    return;
-  }
-  console.log("Connected to database.");
-});
-
-module.exports = conn;
-
 
 conn.connect((err) => {
   if (err) {
